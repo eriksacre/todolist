74020dbc1a2ff57f77af4deb66c0dd8999b718da
Erik Sacre <erik.sacre@gmail.com>
2013-08-16 21:23:30 +0200

    Handle the case where an expired session is detected during an Ajax call

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,17 @@ class ApplicationController < ActionController::Base
     helper_method :current_user
     
     def authorize
-      path = login_url + "?return=" + CGI.escape(request.original_fullpath)
-      redirect_to path, alert: "You must be logged in to access that page" if current_user.nil?
+      redirect_to_login if current_user.nil?
+    end
+    
+    def redirect_to_login
+      if request.xhr?
+        # It would be nicer for the user if we sent an Ajax login form
+        # so the user would remain on the current screen.
+        render js: "alert('Your session expired. Please login to continue working.'); window.location.href = '#{root_url}';"
+      else
+        path = login_url + "?return=" + CGI.escape(request.original_fullpath)
+        redirect_to path, alert: "You must be logged in to access that page"
+      end
     end
 end
